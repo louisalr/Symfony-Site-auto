2a635b1113cc8347347ccaef1f3760423ea0f2fb
Louis Alary <louis.alary34@gmail.com>
2022-03-08 14:29:28 +0100

    Updated project dependancies

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,14 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
-// start the Stimulus application
-import './bootstrap';
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
